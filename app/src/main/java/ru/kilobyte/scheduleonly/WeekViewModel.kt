7bf3c4eb911d77eafc9c48cc4cd2a14a4a6a4824
kilobyte <itskilobyte@gmail.com>
2026-01-07 17:56:33 +0500

    added view model for days

---
+++ app/src/main/java/ru/kilobyte/scheduleonly/WeekViewModel.kt
@@ -0,0 +1,21 @@
+package ru.kilobyte.scheduleonly
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.kilobyte.scheduleonly.data.Day
+import ru.kilobyte.scheduleonly.data.Lesson
+import java.time.LocalTime
+
+class WeekViewModel : ViewModel() {
+    private val _days = MutableLiveData<List<Day>>()
+    val days: LiveData<List<Day>> = _days
+
+    private fun getDays(): List<Day> {
+        return listOf()
+    }
+
+    init {
+        _days.value = getDays()
+    }
+}
